89786b10721d8ac5e69b635eae5e8b4c39813875
Mehdi Pierre DAOU <mehdi-pierre.daou@arteliagroup.com>
2018-01-04 15:44:05 +0100

    update resources.py

--- resources.py
+++ resources.py
@@ -2,7 +2,7 @@
 
 # Resource object code
 #
-# Created: mer. 20. déc. 16:42:37 2017
+# Created: jeu. 4. janv. 15:43:33 2018
 #      by: The Resource Compiler for PyQt (Qt v4.8.6)
 #
 # WARNING! All changes made in this file will be lost!
