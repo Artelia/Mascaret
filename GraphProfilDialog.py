50a226cd917ffffc3a0c7b3e7f4a19a60f7d1933
daoum <daoum@users.noreply.github.com>
2019-02-15 09:11:48 +0100

    fixed display graph profil bug

--- GraphProfilDialog.py
+++ GraphProfilDialog.py
@@ -1743,7 +1743,8 @@ class GraphHydro(GraphCommon):
 
         self.listeRuns = {}
         for run, scen, pk in zip(dico_run["run"], dico_run["scenario"], dico_run['pk']):
-            if type == 't':
+            print(type)
+            if self.type == 't':
                 try:
                     pk_tmp = [round(elem, 2) for elem in pk]
                     idx = pk_tmp.index(self.position)
@@ -1757,6 +1758,7 @@ class GraphHydro(GraphCommon):
                     self.listeRuns[run] = []
                 self.listeRuns[run].append(scen)
 
+        print(self.listeRuns)
         if self.listeRuns == {}:
             self.mgis.add_info('No results for this profile. \n')
         self.run = sorted(self.listeRuns.keys())[-1]
