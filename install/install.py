e9001792bd395cb0f993d8d24e97ef796221466f
Mehdi Pierre DAOU <mehdi-pierre.daou@arteliagroup.com>
2020-01-10 09:58:04 +0100

    Update install.py

--- install/install.py
+++ install/install.py
@@ -17,21 +17,20 @@ email                :
  *                                                                         *
  ***************************************************************************/
  """
-import requests
+
 import os
-from urllib.parse import urljoin
 import posixpath
+from PyQt5.QtCore import QUrl, QEventLoop, QTimer
+from PyQt5.QtNetwork import QNetworkRequest, QNetworkReply
+from qgis.core import QgsNetworkAccessManager
 
 
-class ClassDownloadMasc:
+class ClassDownloadMasc():
+    """
+    Class allowing to download needed files
+    """
     def __init__(self, local_install=None):
         self.masplug_path = local_install
-        self.http_proxy = "http://proxy.arteliagroup.com:3128"
-        self.https_proxy = "http://proxy.arteliagroup.com:3128"
-        self.ftp_proxy = ""
-        self.prox = {"http": self.http_proxy,
-                     "https": self.https_proxy,
-                     "ftp": self.ftp_proxy}
         self.version = 'master'
         if self.version == 'experimental':
             self.branch = 'dev_hyd_struct'
@@ -39,39 +38,74 @@ class ClassDownloadMasc:
             self.branch = 'master'
         self.url_base = 'https://raw.githubusercontent.com/Artelia/Mascaret/'
 
-    def load_bin(self):
+    def douwnload_bin_dir(self):
+        """
+        download "bin" represitory
+        :return:
+        """
+
+        self.manager = QgsNetworkAccessManager()
+        self.manager = self.manager.instance()
+
         rep = 'bin'
         file_list = ['mascaret.exe',
                      'mascaret_linux']
         url_path = posixpath.join(self.branch, rep)
-        url2 = urljoin(self.url_base, url_path)
+        url = posixpath.join(self.url_base, url_path)
 
         os.makedirs(os.path.join(self.masplug_path, rep), exist_ok=True)
+        for filen in file_list:
+            url2 = posixpath.join(url, filen)
+            paht_file = os.path.join(self.masplug_path, rep, filen)
+            self.download(url2, paht_file)
 
-        print(url2, os.path.realpath(rep))
-        self.download_rep(url2, rep, file_list)
-
-    def download_rep(self, url, rep, file_list, typ_w='wb'):
+    def download(self, url, path_file):
         """
-        Download file in directory
-        :param rep
-        :param url:
-        :param file_list:
-        :param typ_w:
+        dowload function
+        :param url: url path of file
+        :param path_file: path to save file
         :return:
         """
-        for filen in file_list:
-            with requests.Session() as s:
-                s.proxies = self.prox
-                result = s.get(urljoin(url, filen), timeout=2)
-                with open(os.path.join(self.masplug_path, rep, filen), typ_w) as fil:
-                    fil.write(result.content)
+        self.file_install = path_file
+        self.url = url
+        self.loop = QEventLoop()
+        timer = QTimer()
+        timer.setSingleShot(True)
+        timer.timeout.connect(lambda: self.loop.exit(1))
+        timer.start(100000)  # 10 second time-out
+        # req = QNetworkRequest(QUrl('https://www.python.org/'))
+        req = QNetworkRequest(QUrl(url))
+        self.result = self.manager.get(req)
+        self.result.finished.connect(self.fin_req)
+        print('fetching request...')
+        if self.loop.exec_() == 0:
+            timer.stop()
+            print('received: ', self.result.readAll().count())
+        else:
+            print('request timed-out :(')
 
-    def main(self):
-        self.load_bin()
+    def fin_req(self):
+        """
+        action when received the request
+        :return:
+        """
+        if self.result.error() != QNetworkReply.NoError:
+            self.loop.exit()
+            return
+        self.loop.exit()
+        # save file
+        self.save_file(self.result.readAll())
+
+    def save_file(self, data):
+        """
+        Save file
+        :param data: data in file
+        :return:
+        """
+        fch = open(self.file_install, 'wb')
+        with fch:
+            fch.write(data)
 
 
 if __name__ == "__main__":
-    cl_load = ClassDownloadMasc()
-    cl_load.branch = 'download_exe'
-    cl_load.main()
+    pass
