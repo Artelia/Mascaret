f7e5c1072558239f07714ccea6d23b34b1952119
DAOU Mehdi Pierre <mehdi-pierre.daou@arteliagroup.com>
2021-08-30 18:10:36 +0200

    fixe  bug
    Pb of ddl recognition

--- api/masc.py
+++ api/masc.py
@@ -134,18 +134,19 @@ class Mascaret:
         # MDU modif
         # libmascaret = './libmascaret.so'
         test = sys.platform
-
         if 'linux' in test or test == 'cygwin':
-            libmascaret = '../api/libmascaret.so'
+            # libmascaret = '../api/libmascaret.so'
+            libmascaret = os.path.join(os.path.dirname(__file__),
+                                       'libmascaret.so')
         elif test == 'win32':
-            libmascaret = '../api/libmascaret.dll'
+            #libmascaret = '../api/libmascaret.dll'
+            libmascaret = os.path.join(os.path.dirname(__file__),'libmascaret.dll')
             # libmascaret = '../api/libmascaret_old.dll'
         else:
             print("{0} platform  doesn't allow to run simulation.".format(test))
             return False
 
         # self.load_mascaret(libmascaret)
-
         self.libmascaret = ctypes.CDLL(libmascaret)
         # MDU fin modif
         self.iprint = 0
