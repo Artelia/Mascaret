06e7a878ae1c63f00ba076bd77a9dbbd3b00224e
daoum <mehdi-pierre.daou@arteliagroup.com>
2026-04-15 17:59:05 +0200

    clear useless python

--- Mascaret/db/sql/style.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO layer_styles(f_table_catalog, f_table_schema, f_table_name, f_geometry_column,
-                         stylename, styleqml, stylesld, useasdefault, description,owner)
-
-(SELECT f_table_catalog, '{0}' as f_table_schema, f_table_name, f_geometry_column,
- stylename, styleqml, stylesld, useasdefault, description,'{1}'
- FROM layer_styles
- WHERE f_table_schema='template')
